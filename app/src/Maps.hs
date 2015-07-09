26696d447e8066430105e6de9c08cad4a12596cb
Dominic 'skapazzo' Wanger <skapazzo@inventati.org>
2015-07-09 17:11:16 +0200

    some work

--- app/src/Maps.hs
+++ app/src/Maps.hs
@@ -1,12 +1,14 @@
 module Maps (Floor(), getSize, getCell, Cell(..),generateMap, standardMap, exampleMap) where
 
 import Utils(Pos)
-import Math.Geometry.Grid
+import Math.Geometry.Grid.Square
+import Math.Geometry.Grid(size)
 
 data Cell = Wall | Empty | Door | StairDown | StairUp | Window | Exit
     deriving (Show, Read)
 
-type Floor = [[Cell]]
+-- type Floor = [[Cell]]
+type Floor = RectSquareGrid Cell
 
 -- | generate standard map
 standardMap = generateMap (repeat 0) 10 20
@@ -15,28 +17,31 @@ standardMap = generateMap (repeat 0) 10 20
 w = Wall
 f = Empty
 exampleMap :: Floor
-exampleMap = [[w,w,w,w,w],[w,f,f,f,w],[w,f,f,f,w],[w,w,w,w,w]]
+exampleMap = undefined
+-- exampleMap = [[w,w,w,w,w],[w,f,f,f,w],[w,f,f,f,w],[w,w,w,w,w]]
 
 -- | generate a floor with specific size
-generateMap :: [Int] -> Int -> Int -> Floor
+-- generateMap :: [Int] -> Int -> Int -> Floor
 generateMap rs = generateRoom z
     where
         z = head rs
 
 generateRoom ::Int -> Int -> Int -> Floor
-generateRoom _ h w = edge : (take (h-2) $ repeat middle) ++ [edge]
-    where
-        edge = take w $ repeat Wall
-        middle = Wall : (take (w-2) $ repeat Empty) ++ [Wall]
+generateRoom = undefined
+-- generateRoom _ h w = edge : (take (h-2) $ repeat middle) ++ [edge]
+    -- where
+        -- edge = take w $ repeat Wall
+        -- middle = Wall : (take (w-2) $ repeat Empty) ++ [Wall]
 
 addExit :: Int -> Int -> Floor -> Floor
 addExit = undefined
 
 -- | get the Cell at Pos
 getCell :: Floor -> Pos -> Cell
-getCell fl (x,y) = fl!!x!!y
+getCell = undefined
+-- getCell fl (x,y) = fl!!x!!y
 
 -- | get the size of a Floor in Pos
-getSize :: Floor -> Pos
-getSize fl = (length fl, length $ head fl)
+-- getSize :: Floor -> Pos
+getSize = size
 
