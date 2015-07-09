1b54ade9e5647254cfb8910bc06d251acb295de9
Dominic 'skapazzo' Wanger <skapazzo@inventati.org>
2015-07-09 15:21:54 +0200

    added TestBed.hs

---
+++ app/src/TestBed.hs
@@ -0,0 +1,19 @@
+import Maps
+import UI
+import Entities
+import Utils
+import Graphics
+import IO
+import Control.Monad(sequence_)
+
+
+-- main :: IO(a->a)
+main = do
+    sequence_ activeTests
+
+-- activeTests :: [IO()]
+activeTests = testDraw:
+              []
+
+-- testDraw :: IO()
+testDraw = putStrLn $ unlines $ draw [exampleEntity] $ standardMap
