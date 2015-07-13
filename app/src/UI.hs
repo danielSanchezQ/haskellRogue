2c3cec33304c0d3f1b82f3460e96db72cd357028
dani <3danimanimal@gmail.com>
2015-07-13 16:24:41 +0200

    ui updates with menu and quit actions

--- app/src/UI.hs
+++ app/src/UI.hs
@@ -4,16 +4,14 @@ import Utils        (Move(..))
 import Graphics     (drawMenu)
 import Data.Char    (digitToInt)
 
-
-data Action a   = Action a  | None              deriving (Show, Eq)
+data Action a   = Action a  | None  | Menu | Quit   deriving (Show, Eq)
 
 instance Monad Action where
     return a          = Action a 
     (Action a)  >>= f = f a
     None        >>= _ = None
 
-
-data Choice     = Accept    | Deny | Choice Int deriving (Show, Eq)
+data Choice     = Accept    | Deny | Choice Int     deriving (Show, Eq)
 
 yesNoChoice     = [Accept, Deny]
 numeralChoice   = [Choice n | n <- [0..9]]
@@ -28,6 +26,8 @@ parseCommand 'w' = Action UP
 parseCommand 's' = Action DOWN
 parseCommand 'a' = Action LEFT
 parseCommand 'd' = Action RIGHT
+parseCommand 'm' = Menu
+parseCommand 'q' = Quit
 parseCommand  _  = None
 
 parseChoice :: Char -> Action Choice
@@ -38,18 +38,17 @@ parseChoice  c  | c `elem` ['0'..'9']   = Action (Choice (digitToInt c))
 
 
 --abstraction for taking commands, run with parse* to get an action. Daniel
-readInput :: (Char -> Action a) -> Menu -> IO (Action a)
+readInput :: (Char -> Action a) -> IO (Action a)
 readInput rf m = do
-    drawMenu m
     c <- getChar
     return (rf c)
-
 
 readCommand = readInput parseCommand
 readChoice  = readInput parseChoice
 
 ask :: Eq a => Menu -> [a] -> (Menu -> IO(Action a)) -> IO(Action a)
 ask m cs rf = do
+    drawMenu m
     e <- rf m
     case e of
         None        -> ask m cs rf
