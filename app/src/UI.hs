9299bafc046a4e08aa93fa9718410f47a97442d2
dani <3danimanimal@gmail.com>
2015-07-13 14:22:14 +0200

    asking fucntion fixed and working

--- app/src/UI.hs
+++ app/src/UI.hs
@@ -5,7 +5,7 @@ import Graphics     (drawMenu)
 import Data.Char    (digitToInt)
 
 
-data Action a   = Action a  | None              deriving (Show)
+data Action a   = Action a  | None              deriving (Show, Eq)
 
 instance Monad Action where
     return a          = Action a 
@@ -13,7 +13,7 @@ instance Monad Action where
     None        >>= _ = None
 
 
-data Choice     = Accept    | Deny | Choice Int deriving (Show)
+data Choice     = Accept    | Deny | Choice Int deriving (Show, Eq)
 
 yesNoChoice     = [Accept, Deny]
 numeralChoice   = [Choice n | n <- [0..9]]
@@ -33,18 +33,29 @@ parseCommand  _  = None
 parseChoice :: Char -> Action Choice
 parseChoice 'y' = Action Accept
 parseChoice 'n' = Action Deny
-parseChoice  c  | digitToInt c `elem` [0..9] = Action (Choice (digitToInt c))
+parseChoice  c  | c `elem` ['0'..'9']   = Action (Choice (digitToInt c))
+                | otherwise             = None
 
 
 --abstraction for taking commands, run with parse* to get an action. Daniel
-readInput :: (Char -> Action a) -> IO (Action a)
-readInput rf = do 
+readInput :: (Char -> Action a) -> Menu -> IO (Action a)
+readInput rf m = do
+    drawMenu m
     c <- getChar
     return (rf c)
 
 
---ask :: Menu -> [Choice] -> Action Choice
---ask m cs = do
---    drawMenu m
---    ret@(Action c) <- readInput parseChoice
---    if c `elem` cs then ret else ask m cs
+readCommand = readInput parseCommand
+readChoice  = readInput parseChoice
+
+ask :: Eq a => Menu -> [a] -> (Menu -> IO(Action a)) -> IO(Action a)
+ask m cs rf = do
+    e <- rf m
+    case e of
+        None        -> ask m cs rf
+        otherwise   -> if e `elem` [Action x | x <- cs] then return e else ask m cs rf
+            
+
+
+----ask "asndfoasidhfaspodifh" [Accept, Deny] readChoice
+
