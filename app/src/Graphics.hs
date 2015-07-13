fc095cf04760e3984f394dec0306913eb86ba76a
Dominic 'skapazzo' Wanger <skapazzo@inventati.org>
2015-07-13 11:29:03 +0200

    Bugfix: x, y switched in entity drawing

--- app/src/Graphics.hs
+++ app/src/Graphics.hs
@@ -14,11 +14,11 @@ draw es m = foldr overlayEs emptyMap es
 -- ugly and inefficient, ToDo: change to cps or anything better. skapazzo
 -- ToDo: check for out of bounds coordinates
 overlayEs :: Entity -> [String] -> [String]
-overlayEs e cs = take (x-1) cs ++ (overLayEs2 y (cs!!x): drop x cs)
+overlayEs e cs = take (y-1) cs ++ (overLayEs2 x (cs!!y): drop y cs)
     where
         (x,y) = getPosition e
         overLayEs2 :: Int -> String -> String
-        overLayEs2 y cs = take (x-1) cs ++ (char : drop x cs)
+        overLayEs2 x cs = take (x-1) cs ++ (char : drop x cs)
         char = drawEntity e
 
 changeEs :: Entity -> (Int, Int, Char)
