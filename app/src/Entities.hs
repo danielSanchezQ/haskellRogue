b2b5afaefb2ce68ee9c1b8b5a1e26a9a7abc1f97
Dominic 'skapazzo' Wanger <skapazzo@inventati.org>
2015-07-13 14:20:44 +0200

    added races to entities

--- app/src/Entities.hs
+++ app/src/Entities.hs
@@ -1,9 +1,10 @@
 --HASKELL ROGUE GAME MODULE ENTITIES--
 module Entities  where
-import Utils (Pos, Move(..), makeMove')
+import Utils (Pos, Move(..), makeMove', movePos)
 
-data Job    =  Mage | Healer | Assassin | Barbarian | Nojob         deriving (Show)
+data Job    =  Mage | Healer | Assassin | Barbarian | NoJob         deriving (Show)
 data WType  =  Sword | Bow | Rod | Magic                            deriving (Show)
+data Race   =  Hero | Human | Troll | Dragon | Elven | Feline | Dwarven    deriving (Show,Eq)
 
 data Weapon =  Weapon { wname   :: String, 
                         wpower  :: Int, 
@@ -13,14 +14,15 @@ data Entity =  Entity { ename    :: String,
                         elifes   :: Int,
                         ejob     :: Job, 
                         eweapon  :: Weapon,
-                        eposition:: Pos}                            deriving (Show)
+                        eposition:: Pos,
+                        erace    :: Race }                            deriving (Show)
 
 
 type Hero       = Entity
 type Monster    = Entity
 
 attack :: Entity -> Entity -> (Entity, Entity)
-attack hero@(Entity hname hlifes hjob hweapon _)  monster@(Entity mname mlifes mjob mweapon _) = undefined
+attack hero@(Entity hname hlifes hjob hweapon _ _)  monster@(Entity mname mlifes mjob mweapon _ _) = undefined
 
 pickWeapon :: Hero -> Weapon -> Entity
 pickWeapon e w = e {eweapon = w}
@@ -39,12 +41,17 @@ hit e n = e {elifes = remained}
 moveEntity :: Entity -> Move -> Entity
 moveEntity e m = e {eposition = makeMove' (eposition e) m}
 
+moveEntityP :: Entity -> Pos -> Entity
+moveEntityP e pos = e {eposition = movePos pos (eposition e)}
+
+getRace :: Entity -> Race
+getRace = erace
+
 getPosition :: Entity -> Pos
 getPosition = eposition
 
-
 exampleEntity :: Entity
-exampleEntity = Entity [] 1 Mage exampleWeapon (2,3)
+exampleEntity = Entity [] 1 Mage exampleWeapon (2,3) Elven
 
 exampleWeapon :: Weapon
 exampleWeapon = Weapon "example weapon" 1 Rod
