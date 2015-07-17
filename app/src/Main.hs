1c2ad273ee4f3fb6e1ef84ddd4a6ebd8695a0de9
Dominic 'skapazzo' Wanger <skapazzo@inventati.org>
2015-07-17 18:08:27 +0200

    fixed bug: new enemies not in random positions

--- app/src/Main.hs
+++ app/src/Main.hs
@@ -5,34 +5,30 @@ import Utils
 import Graphics
 import Logic
 import System.IO
-import System.Random (getStdGen,RandomGen)
+import System.Random (getStdGen,RandomGen,split)
 
-
-
---using ranGen wrong
 gameLoop :: RandomGen g => (GameState, g) -> IO()
 gameLoop (gameState, ranGen) = do
         if (getHealth $ getHero $ gameState) <= 0 then do
             choice <- ask "\nYou died!!\nWant to try again y/n" yesNoChoice
             case choice of
-                Accept      -> gameLoop (myGame ranGen, ranGen)
+                Accept      -> gameLoop (myGame ran1, ran2)
                 Deny        -> putStrLn "Goodbye" >> return ()
         else do
-            putStrLn "---------------------\n"
             clearAndDraw draw gameState
             command <- readCommand
-            -- print command
             case command of
                 (NoAction)  -> gameLoop (gameState, ranGen)
                 (Quit)      -> do   putStrLn "Goodbye"
                                     return ()
-                (TA a)      -> gameLoop (step ranGen a gameState, ranGen)
+                (TA a)      -> gameLoop (step ran1 a gameState, ran2)
                 otherwise   -> do print $ "Unexpected command:" ++ (show command)
                                   gameLoop (gameState, ranGen)
+        where
+            (ran1,ran2) = split ranGen
 
 myGame :: RandomGen g => g -> GameState
 myGame ranGen = newGame ranGen
-    where
 
 main :: IO()
 main = do
